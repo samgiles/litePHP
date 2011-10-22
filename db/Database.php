0f6eaa77aee29046fab034b30228251fd160b461
Sam Giles <sam@sam-giles.co.ukk>
2011-10-22 03:25:02 +0100

    database class

---
+++ db/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+ 	protected $_dbHandle;
+	 protected static $_this;
+	 
+	 public static function GetHandle(){
+	 	if (is_null(Database::$_this)){
+	 		Database::$_this = new Database();
+	 	}
+	 	
+	 	return Database::$_this->getAHandle();
+	 }
+	 
+	 private function __construct($dbhost, $dbname, $uname, $password){
+	 	$this->_dbHandle = new PDO("mysql:host=$dbhost;dbname=$dbname", "$uname", "$password"); 
+	 }
+	 
+	 private function getAHandle(){
+	 	return $this->_dbHandle;
+	 }
+}
