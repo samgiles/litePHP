774cd91f297c2aaa3a5d6ff960cab0e70c8ca29b
Sam Giles <sam@sam-giles.co.ukk>
2011-10-22 03:25:20 +0100

    Session storage

---
+++ session/session_store.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+/**
+ * Base class for storing session information.
+ * @author Sam
+ *
+ */
+class session_store {
+	
+	/**
+	 * Session key
+	 * @var unknown_type
+	 */
+	protected $_key;
+	
+}
